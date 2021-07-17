e7c4311e0e1c00c72dcb85948f402a7fb26d58f3
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-07-17 23:12:14 +0530

    cording submit button in second question window

--- ProjectFinal/src/projectfinal/secondquestion.java
+++ ProjectFinal/src/projectfinal/secondquestion.java
@@ -5,11 +5,21 @@
  */
 package projectfinal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author DELL
  */
 public class secondquestion extends javax.swing.JFrame {
+    Connection con= null;
+    PreparedStatement pst=null;
+    ResultSet rs = null;
+    String value;
 
     /**
      * Creates new form secondquestion
@@ -49,21 +59,46 @@ public class secondquestion extends javax.swing.JFrame {
         buttonGroup1.add(jRadioButton1);
         jRadioButton1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jRadioButton1.setText("Colombo");
+        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton2);
         jRadioButton2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jRadioButton2.setText("Galle");
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton3);
         jRadioButton3.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jRadioButton3.setText("Sri Jayawardhanapura Kotte");
+        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton3ActionPerformed(evt);
+            }
+        });
 
         buttonGroup1.add(jRadioButton4);
         jRadioButton4.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jRadioButton4.setText("Kandy");
+        jRadioButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton4ActionPerformed(evt);
+            }
+        });
 
         jButton1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jButton1.setText("Submit Answer");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jButton2.setText("Check Answer");
@@ -128,6 +163,52 @@ public class secondquestion extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+        value = "Colombo";
+    }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+        value = "Galle";
+    }//GEN-LAST:event_jRadioButton2ActionPerformed
+
+    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
+        value = "Sri Jayawardhanapura Kotte";
+    }//GEN-LAST:event_jRadioButton3ActionPerformed
+
+    private void jRadioButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton4ActionPerformed
+        value = "Kandy";
+    }//GEN-LAST:event_jRadioButton4ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try{
+           String submitQuery ="INSERT INTO `submittedanswer`(`sa`) VALUES (?)";
+           con = DriverManager.getConnection("jdbc:mysql://localhost/myquiz", "root", "");
+           pst =con.prepareStatement(submitQuery);
+           pst.setString(1, value);
+           pst.executeUpdate();
+           JOptionPane.showMessageDialog(this, "Your Answer is submitted Successfull");
+           String extractQuery="select submittedanswer.sa, answer.ans from answer inner join (select sa from submittedanswer order by id desc limit 1) as submittedanswer on submittedanswer.sa= answer.ans";
+           pst= con.prepareStatement(extractQuery);
+           rs=pst.executeQuery();
+           if(rs.next()){
+               
+           String valueQuery = "INSERT INTO Compare(value) VALUES(?)";
+           pst=con.prepareStatement(valueQuery);
+           pst.setString(1, "Correct");
+           pst.executeUpdate();}
+           else{
+           String valueQuery = "INSERT INTO compare(value) VALUES (?)";
+           pst= con.prepareStatement(valueQuery);
+           pst.setString(1, "INCORRECT");
+           pst.executeUpdate();}
+           
+           
+       }
+       catch(Exception ex){
+           
+       JOptionPane.showMessageDialog(this, ex.getMessage());}
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
