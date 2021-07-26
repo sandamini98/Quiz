4f5641710e6e6d30dbec0587e80ff113f3c710a3
ChandiSandamini <96458@fhss.sjp.ac.lk>
2021-07-26 14:40:45 +0530

    continue button connect to the next window

--- ProjectFinal/src/projectfinal/roundone.java
+++ ProjectFinal/src/projectfinal/roundone.java
@@ -44,6 +44,11 @@ public class roundone extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
         jButton1.setText("CONTINUE");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -88,6 +93,12 @@ public class roundone extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        firstquestion sc=new firstquestion();
+        new firstquestion().setVisible(false);
+        sc.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
